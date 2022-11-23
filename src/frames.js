41934adbc12957fc14a58b91bab93eda317e95de
jacknotman <jacknotman@gmail.com>
2022-11-23 10:37:13 +0000

    Create frames.js

---
+++ src/frames.js
@@ -0,0 +1,33 @@
+class Frames {
+  //
+	animate() {
+		return new Promise(resolve => {
+      const self = this;
+      const start = Date.now();
+			const length = this.frames.length;
+			let index = 0;
+			const animationSchedule = async () => {
+				let currentFrame = self.frames.shift();
+				if(currentFrame) {
+					self.framesDone.push(currentFrame);
+					Promise.resolve(self.animationFunction(currentFrame, index++, length)).then(x => window.requestAnimationFrame(animationSchedule));
+				} else {
+					Promise.all(self.framesDone).then(values => {
+						self.frames = self.framesDone;
+						self.framesDone = [];
+						resolve([self, Date.now() - start]);
+					});
+				}
+			}
+			window.requestAnimationFrame(animationSchedule);
+		});
+	}
+	//
+	constructor(frames, animation) {
+    this.framesDone = [];
+		this.frames = frames;
+		this.animationFunction = animation;
+	}
+}
+
+export default Frames;
