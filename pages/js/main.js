9ef5810e80c11ceb80792cd7023f7d8545e73198
jacknotman <jacknotman@gmail.com>
2022-11-23 10:44:37 +0000

    Create main.js

---
+++ pages/js/main.js
@@ -0,0 +1,41 @@
+//
+// Setup
+//
+
+// Import Anima
+import { default as Frames } from "./Frames.js";
+
+// Useful Hack for ios safari setting a CSS variable equal to the active viewport height.
+const appHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight();
+
+//
+// Main
+//
+
+// Create an array of characters for our animation to use.
+const characterArray2 = 'Hello World, \nWelcome to Frames. \n\nThe animation scheduling protocol for JS. \n\n488 bytes of code.\n275 gZipped.\nAwesome.'.split('');
+
+// Create an element for our animations
+const element2 = document.createElement('span');
+element2.ariaLive = "assertive"
+element2.ariaBusy = "true";
+document.querySelector('.content').appendChild(element2);
+
+// Loops over the above character array, printing a character approximately 
+// every 40 miliseconds. 
+new Frames(characterArray2, frame => {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			element2.innerHTML += frame;
+			resolve();
+		}, 40);
+	});
+}).animate().then(([self, time]) => {
+  element2.ariaBusy = "false";
+  console.log(`Done in: ${time}ms`);
+});
